47ed4587fcbc426d960b3d04e4075c5f757c28e3
mamoo <m.pedriale@gmail.com>
2012-09-11 09:45:28 +0200

    First commit.
    Added js lib + test lib + dummy spec.

---
+++ test/spec/dummySpec.js
@@ -0,0 +1,16 @@
+describe("Player", function() {
+
+  beforeEach(function() {
+
+  });
+
+  describe("a simple test", function() {
+    it("should pass", function() {
+      expect(true).toBeTruthy();
+    });
+
+    it("should fail", function() {
+      expect(true).toBe(false);
+    });
+  });
+});
---
+++ test/specrunner.js
@@ -0,0 +1,65 @@
+require.config({
+  /*baseUrl: "/js/",*/
+  urlArgs: 'cb=' + Math.random(),
+  paths: {
+    jquery: '../lib/jquery.min',
+    underscore: '../lib/underscore-min',
+    backbone: '../lib/backbone-min',
+    jasmine: './lib/jasmine-1.2.0/jasmine',
+    'jasmine-html': './lib/jasmine-1.2.0/jasmine-html',
+    spec: './spec/'
+  },
+  shim: {
+    underscore: {
+      exports: "_"
+    },
+    backbone: {
+      deps: ['underscore', 'jquery'],
+      exports: 'Backbone'
+    },
+    'backbone.localStorage': {
+      deps: ['backbone'],
+      exports: 'Backbone'
+    },
+    jasmine: {
+      exports: 'jasmine'
+    },
+    'jasmine-html': {
+      deps: ['jasmine'],
+      exports: 'jasmine'
+    }
+  }
+});
+
+require(['underscore', 'jquery', 'jasmine-html'], function(_, $, jasmine){
+ 
+  var jasmineEnv = jasmine.getEnv();
+  jasmineEnv.updateInterval = 1000;
+ 
+  var htmlReporter = new jasmine.HtmlReporter();
+ 
+  jasmineEnv.addReporter(htmlReporter);
+ 
+  jasmineEnv.specFilter = function(spec) {
+    return htmlReporter.specFilter(spec);
+  };
+ 
+  var specs = [];
+ 
+  specs.push('spec/dummySpec');
+  /*specs.push('spec/views/ClearCompletedSpec');
+  specs.push('spec/views/CountViewSpec');
+  specs.push('spec/views/FooterViewSpec');
+  specs.push('spec/views/MarkAllSpec');
+  specs.push('spec/views/NewTaskSpec');
+  specs.push('spec/views/TaskListSpec');
+  specs.push('spec/views/TaskViewSpec');*/
+ 
+ 
+  $(function(){
+    require(specs, function(){
+      jasmineEnv.execute();
+    });
+  });
+ 
+});
